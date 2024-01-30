65fa80adddf737289639d9740268a27a4f56f174
Emilien Jegou <emj339@gmail.com>
2024-01-30 15:43:59 +0100

    feat: better mobile layout on homepage

--- src/ui/layout/blog-card/index.tsx
+++ src/ui/layout/blog-card/index.tsx
@@ -13,16 +13,18 @@ type BlogCardProps = {
 
 export const BlogCard = (props: BlogCardProps) => (
   <Link href={props.slug}>
-    <div class={twMerge('flex items-center gap-20', props.class)}>
+    <div class={twMerge('flex items-center gap-8 sm:gap-20', props.class)}>
       <div>
         <p class="text-subtler text-sm">
           {formatDistance(props.date, new Date(), { addSuffix: true })}
         </p>
         <h3 class="font-medium mt-1">{props.title}</h3>
-        <p class="leading-6 mt-2 text-subtle text-sm">{props.description}</p>
+        <p class="leading-6 mt-2 text-subtle text-sm max-w-[480px] sm:max-w-auto pr-6">
+          {props.description}
+        </p>
         <p class="block mt-2 font-medium underline text-sm">Read now</p>
       </div>
-      <div class="shrink-0">
+      <div class="hidden sm:block shrink-0">
         <img
           class="object-cover rounded-2xl w-[122px] shadow h-[122px]"
           src={props.thumbnailUrl}
