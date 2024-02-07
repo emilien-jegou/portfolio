5573018de98a834599a9a711f703d8f369fc7403
Emilien Jegou <emj339@gmail.com>
2024-02-07 06:18:02 +0100

    feat: rss button in header bar

--- src/ui/layout/header-bar/index.tsx
+++ src/ui/layout/header-bar/index.tsx
@@ -1,7 +1,8 @@
 import { ExternalLink } from '~/ui/common/external-link';
 
 export const HeaderBar = () => (
-  <header class="absolute right-4 top-4">
+  <header class="absolute flex gap-4 right-4 top-4">
+    <ExternalLink href="https://emje.dev/rss.xml" label="RSS" disableIcon />
     <ExternalLink href="https://github.com/emilien-jegou" label="Github" />
   </header>
 );
